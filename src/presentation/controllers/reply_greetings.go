1738b1fd6ae1f3db86c7098995ffacce41237c62
Lucas William <lucaswilliameufrasio@gmail.com>
2021-01-22 03:29:18 -0300

    refactor: rename import namespace

--- src/presentation/controllers/reply_greetings.go
+++ src/presentation/controllers/reply_greetings.go
@@ -1,10 +1,10 @@
 package controllers
 
-import presentationprotocols "lucaswilliameufrasio/golang-fiber-api/src/presentation/protocols"
+import protocols "lucaswilliameufrasio/golang-fiber-api/src/presentation/protocols"
 
 // ReplyGreetingsController is a controller to handle request and respond with a great message
-func ReplyGreetingsController(request *presentationprotocols.HTTPRequest) presentationprotocols.HTTPResponse {
-	return presentationprotocols.HTTPResponse{
+func ReplyGreetingsController(request *protocols.HTTPRequest) protocols.HTTPResponse {
+	return protocols.HTTPResponse{
 		StatusCode: 200,
 		Data: map[string]interface{}{
 			"data": "Hello, World 👋!",
