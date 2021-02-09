4a6d68bc4331e844900cf2d2985e103234a9e404
Lucas William <lucaswilliameufrasio@gmail.com>
2021-02-09 13:37:36 -0300

    chore: add custom middleware to validate authentication

--- src/main/middlewares/auth.go
+++ src/main/middlewares/auth.go
@@ -1,7 +1,11 @@
 package middlewares
 
 import (
+	"errors"
+	"fmt"
+	"log"
 	"lucaswilliameufrasio/golang-fiber-api/src/main/config/environment"
+	"strings"
 
 	jwt "github.com/form3tech-oss/jwt-go"
 	"github.com/gofiber/fiber/v2"
@@ -14,6 +18,52 @@ func handleAuthError(c *fiber.Ctx, err error) error {
 	})
 }
 
+func GetTokenFromHeader(c *fiber.Ctx) (string, error) {
+	tokenOnHeader := strings.Split(string(c.Request().Header.Peek("Authorization")), " ")
+
+	if len(tokenOnHeader) == 2 {
+		return tokenOnHeader[1], nil
+	}
+
+	return "", errors.New("Can't find any token")
+}
+
+func AuthenticationMiddleware() func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		tokenFromHeader, err := GetTokenFromHeader(c)
+		if err != nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "Unauthorized",
+			})
+		}
+
+		type JWTClaims struct {
+			UserID string `json:"userID"`
+			jwt.StandardClaims
+		}
+
+		token, err := jwt.ParseWithClaims(tokenFromHeader, &JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
+			return []byte(environment.JWT_SECRET), nil
+		})
+
+		if token.Valid {
+			if claims, ok := token.Claims.(*JWTClaims); ok {
+				fmt.Printf("%v\n", claims.UserID)
+				return c.Next()
+			} else {
+				log.Fatal(err)
+				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+					"error": "Unauthorized",
+				})
+			}
+		}
+
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Unauthorized",
+		})
+	}
+}
+
 // AuthenticationRequired is a middleware to verify the token validation
 func AuthenticationRequired() func(c *fiber.Ctx) error {
 	var jwtConfig = jwtware.Config{
@@ -24,23 +74,27 @@ func AuthenticationRequired() func(c *fiber.Ctx) error {
 }
 
 // AuthorizationMiddleware is a middleware to ensure user has a role setted
-func AuthorizationMiddleware() func(*fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-		var role string
+// func AuthorizationMiddleware() func(*fiber.Ctx) error {
+// 	return func(c *fiber.Ctx) error {
+// 		var role string
 
-		var requestScopeVariable = c.Locals("user")
-		if requestScopeVariable == nil {
-			return c.SendStatus(fiber.StatusUnauthorized)
-		}
-		var userFromContext = c.Locals("user").(*jwt.Token)
-		var claims = userFromContext.Claims.(jwt.MapClaims)
+// 		var requestScopeVariable = c.Locals("user")
+// 		if requestScopeVariable == nil {
+// 			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+// 				"error": "Unauthorized",
+// 			})
+// 		}
+// 		var userFromContext = c.Locals("user").(*jwt.Token)
+// 		var claims = userFromContext.Claims.(jwt.MapClaims)
 
-		role = claims["role"].(string)
+// 		role = claims["role"].(string)
 
-		if role == "guest" {
-			return c.SendStatus(fiber.StatusUnauthorized)
-		}
+// 		if role == "guest" {
+// 			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+// 				"error": "Unauthorized",
+// 			})
+// 		}
 
-		return c.Next()
-	}
-}
+// 		return c.Next()
+// 	}
+// }
