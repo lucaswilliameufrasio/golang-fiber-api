b8cfdb63a1daee88e58464c9df474921c794d336
Lucas William <lucaswilliameufrasio@gmail.com>
2021-02-09 16:11:24 -0300

    refactor: change HttpRequest to include Token and UserID

--- src/presentation/protocols/http.go
+++ src/presentation/protocols/http.go
@@ -5,12 +5,13 @@ type getParams func(string, ...string) string
 // HTTPResponse fields
 type HTTPResponse struct {
 	StatusCode int
-	Data       map[string]interface{}
+	Data       interface{}
 }
 
 // HTTPRequest fields
 type HTTPRequest struct {
 	Body   interface{}
 	Params getParams
-	User   map[string]string
+	UserID *int
+	Token  string
 }
