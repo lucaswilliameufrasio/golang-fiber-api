0aac7ce30668531b99cf62af54e9d0d32238d110
Lucas William <lucaswilliameufrasio@gmail.com>
2021-02-09 16:07:46 -0300

    refactor: use the auth middleware factory

--- src/main/routes/protected.go
+++ src/main/routes/protected.go
@@ -3,12 +3,12 @@ package routes
 import (
 	"lucaswilliameufrasio/golang-fiber-api/src/main/adapters"
 	fctrls "lucaswilliameufrasio/golang-fiber-api/src/main/factories/controllers"
-	"lucaswilliameufrasio/golang-fiber-api/src/main/middlewares"
+	fmdlwrs "lucaswilliameufrasio/golang-fiber-api/src/main/factories/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 // Protected setup
 func Protected(router fiber.Router) {
-	router.Get("/protected", middlewares.AuthenticationMiddleware(), adapters.AdaptRoute(fctrls.MakeProtectedControler()))
+	router.Get("/protected", adapters.AdaptMiddleware(fmdlwrs.MakeAuthMiddleware()), adapters.AdaptRoute(fctrls.MakeProtectedControler()))
 }
