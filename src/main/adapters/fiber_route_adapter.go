9c1833f1a044f503ff4ffee04d513b59668d11d2
Lucas William <lucaswilliameufrasio@gmail.com>
2021-02-09 16:12:45 -0300

    refactor: change AdaptRoute to pass UserID to request

--- src/main/adapters/fiber_route_adapter.go
+++ src/main/adapters/fiber_route_adapter.go
@@ -3,7 +3,6 @@ package adapters
 import (
 	protocols "lucaswilliameufrasio/golang-fiber-api/src/presentation/protocols"
 
-	jwt "github.com/form3tech-oss/jwt-go"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -11,40 +10,28 @@ import (
 func AdaptRoute(controller protocols.Controller) func(*fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
 		var body interface{}
+		var userID *int
 		if err := c.BodyParser(&body); err != nil {
 			c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"error": "Cannot parse json",
+				"error": "Cannot parse body",
 			})
+		}
+
+		id, ok := c.Locals("userID").(int)
+		if !ok {
+			userID = nil
+		} else {
+			userID = &id
 		}
 
 		var request = protocols.HTTPRequest{
 			Body:   body,
 			Params: c.Params,
-			User:   getUserFromContext(c),
+			UserID: userID,
 		}
 
 		var response = controller.Handler(&request)
 
 		return c.Status(response.StatusCode).JSON(response.Data)
-	}
-}
-
-func getUserFromContext(c *fiber.Ctx) map[string]string {
-	var id string
-	var name string
-	var role string
-
-	var requestScopeVariable = c.Locals("user")
-	if requestScopeVariable != nil {
-		var userFromContext = c.Locals("user").(*jwt.Token)
-		var claims = userFromContext.Claims.(jwt.MapClaims)
-
-		role = claims["role"].(string)
-	}
-
-	return map[string]string{
-		"id":   id,
-		"name": name,
-		"role": role,
 	}
 }
