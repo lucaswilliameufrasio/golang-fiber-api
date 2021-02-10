067514c47c96ae2b739a7def6403ec2d272b7b6d
Lucas William <lucaswilliameufrasio@gmail.com>
2021-02-10 13:34:02 -0300

    feat: add graceful shutdown to close connections of whatever when application is terminated

--- src/main/server.go
+++ src/main/server.go
@@ -5,12 +5,27 @@ import (
 	"log"
 	"lucaswilliameufrasio/golang-fiber-api/src/main/config"
 	"lucaswilliameufrasio/golang-fiber-api/src/main/config/environment"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	server := config.App()
 
-	if err := server.Listen(fmt.Sprintf(":%v", environment.Port)); err != nil {
-		log.Panic(err)
-	}
+	go func() {
+		if err := server.Listen(fmt.Sprintf(":%v", environment.Port)); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	channel := make(chan os.Signal, 1)
+	signal.Notify(channel, os.Interrupt, syscall.SIGTERM)
+
+	_ = <-channel
+	fmt.Println("Gracefully shutting down...")
+	_ = server.Shutdown()
+	fmt.Println("Closing connections...")
+	// Put tasks to close connections or whatever needed down here
+	// gormhelpers.Disconnect()
 }
