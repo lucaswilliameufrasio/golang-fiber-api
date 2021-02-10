ea062d71ce9d3fb6a785f1256e6b3d41ead923ff
Lucas William <lucaswilliameufrasio@gmail.com>
2021-02-10 13:43:10 -0300

    refactor: use the SimpleMiddleware only in the websocket route

--- src/main/config/routes.go
+++ src/main/config/routes.go
@@ -17,14 +17,14 @@ func SetupRoutes(app *fiber.App) fiber.Router {
 
 	setupRoutesV1(router)
 
-	router.Get("/ws", websocket.New(events.WebsocketHandler))
+	router.Get("/ws", middlewares.SimpleMiddleware, websocket.New(events.WebsocketHandler))
 
 	return router
 }
 
 // SetupRoutesV1 is a function to export app version 1 routes
 func setupRoutesV1(router fiber.Router) fiber.Router {
-	v1 := router.Group("/v1", middlewares.SimpleMiddleware)
+	v1 := router.Group("/v1")
 
 	routes.LoginRoutes(v1)
 	routes.Protected(v1)
